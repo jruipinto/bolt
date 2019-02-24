8cc7d39df1591273b5dbcbbfedf4207b233c4ddf
J Rui Pinto <rui.jruipinto@gmail.com>
2019-02-24 13:27:29 +0000

    concluida implementação de sidebar responsiva

--- projecto/frontend2/src/app/dashboard/dashboard.component.ts
+++ projecto/frontend2/src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { AuthService } from '../shared/services/auth.service';
 })
 export class DashboardComponent implements OnInit {
   public userName: string;
+  public sidebarVisible = false;
 
   constructor(private authService: AuthService) { }
 
@@ -17,6 +18,14 @@ export class DashboardComponent implements OnInit {
 
   logOut() {
     return this.authService.logOut();
+  }
+
+  toogleSidebar () {
+    if (!this.sidebarVisible) {
+      this.sidebarVisible = true;
+    } else {
+      this.sidebarVisible = false;
+    }
   }
 
 }
