41ca32296d80471e66638d6aa6f191f374e30a01
jruipinto <44852805+jruipinto@users.noreply.github.com>
2019-03-25 14:15:08 +0000

    continuação do comit anterior

--- projecto/frontend3/src/app/pages/dashboard/pages/assistencias/assistencias.state.ts
+++ projecto/frontend3/src/app/pages/dashboard/pages/assistencias/assistencias.state.ts
@@ -4,7 +4,6 @@ import { patch, updateItem } from '@ngxs/store/operators';
 import { DataService, FeathersService } from 'src/app/shared';
 import { Injector } from '@angular/core';
 import { Assistencia } from 'src/app/shared/models';
-import { first } from 'rxjs/operators';
 
 export interface AssistenciaStateModel {
     assistencias: Assistencia[];
@@ -12,12 +11,11 @@ export interface AssistenciaStateModel {
 /* Actions */
 export class PullAssistencias {
     static readonly type = '[Assistencias] Pull from api';
-    constructor(public assistencias: Assistencia[]) { }
 }
 
 export class PatchAssistencias {
-    static readonly type = '[Assistencias] received patch';
-    constructor(public assistencias: Assistencia[]) { }
+    static readonly type = '[Assistencias] received patch from api';
+    constructor(public assistencia: Assistencia) { }
 }
 /* ###### */
 @State<AssistenciaStateModel | null>({
@@ -25,55 +23,72 @@ export class PatchAssistencias {
     defaults: null
 })
 export class AssistenciasState implements NgxsOnInit {
-    private static dataService: DataService;
     private static feathersService: FeathersService;
 
     constructor(injector: Injector) {
-        AssistenciasState.dataService = injector.get<DataService>(DataService);
         AssistenciasState.feathersService = injector.get<FeathersService>(FeathersService);
     }
 
-    @Receiver({ action: [PullAssistencias, PatchAssistencias] })
-    public static setValue({ setState }: StateContext<AssistenciaStateModel>, action: PullAssistencias | PatchAssistencias) {
-        const assistencias = action.assistencias;
-        assistencias.forEach((assistencia, index) => { // preenche o nome de cliente em cada assistencia do array
-            AssistenciasState.dataService.get$('users', assistencia.cliente_user_id)
-                .pipe(
-                    first()
-                )
-                .subscribe(e => {
-                    Object.assign(assistencias[index], { cliente_user_name: e.nome });
-                });
-        });
-        if (action instanceof PullAssistencias) {
-            setState({ assistencias: assistencias });
-        } else if (action instanceof PatchAssistencias) {
-            setState(
-                patch({
-                    assistencias: updateItem(assistencia => assistencia.id === assistencias[0].id, assistencias[0])
-                })
-            );
-        }
-    }
-
-    ngxsOnInit({dispatch}: StateContext<AssistenciaStateModel>) {
+    @Receiver({ action: [PatchAssistencias] })
+    public static setValue({ setState, getState }: StateContext<AssistenciaStateModel>, action: PatchAssistencias) {
         AssistenciasState.feathersService
-            .service('assistencias')
-            .find( {query: {$limit: 25} } )
+            .service('users')
+            .get(action.assistencia.cliente_user_id)
             .then(
-                u => { dispatch(new PullAssistencias(u.data)); },
+                apiUser => {
+                    Object.assign(action.assistencia, { cliente_user_name: apiUser.nome });
+                    setState(
+                        patch({
+                            assistencias: updateItem(stateAssistencia => stateAssistencia.id === action.assistencia.id, action.assistencia)
+                        })
+                    );
+                },
                 err => console.log('error:', err)
             )
         ;
+    }
+
+    @Receiver({ action: PullAssistencias })
+    public static setValue2({ getState, setState }: StateContext<AssistenciaStateModel>) {
         AssistenciasState.feathersService
             .service('assistencias')
-            .on('created', assistencia => { console.log('created: ', assistencia); })
-        ;
+            .find({ query: { $limit: 25 } })
+            .then(
+                u => {
+                    setState({ assistencias: u.data });
+                    getState().assistencias.forEach((assistencia, index) => { // preenche o nome de cliente em cada assistencia do State
+                        AssistenciasState.feathersService
+                            .service('users')
+                            .get(assistencia.cliente_user_id)
+                            .then(
+                                apiUser => {
+                                    Object.assign(assistencia, { cliente_user_name: apiUser.nome });
+                                    setState(
+                                        patch({
+                                            assistencias: updateItem(stateAssistencia => stateAssistencia.id === assistencia.id, assistencia)
+                                        })
+                                    );
+                                },
+                                err => console.log('error:', err)
+                            )
+                            ;
+                    });
+                },
+                err => console.log('error:', err)
+            )
+            ;
+    }
+
+    ngxsOnInit({ dispatch }: StateContext<AssistenciaStateModel>) {
+        dispatch(new PullAssistencias());
+        AssistenciasState.feathersService
+            .service('assistencias')
+            .on('created', assistencia => { dispatch(new PatchAssistencias(assistencia)); })
+            ;
         AssistenciasState.feathersService
             .service('assistencias')
             .on('patched', assistencia => { console.log('patched: ', assistencia); })
-        ;
-
+            ;
     }
 
 }
