879d4c3ba0cda3a245c485c513d60d987f79440e
J Rui Pinto <rui.jruipinto@gmail.com>
2019-03-27 22:40:47 +0000

    developing painel-rapido.state

---
+++ projecto/frontend3/src/app/pages/dashboard-page/pages/painel-rapido-page/painel-rapido-page.state.ts
@@ -0,0 +1,39 @@
+import { State } from '@ngxs/store';
+import { Encomenda, Assistencia } from 'src/app/shared/models';
+
+export interface PainelRapidoPageStateModel {
+    encomendas: Encomenda[];
+    orcamendos: Assistencia[];
+    pedidosContactoCliente: Assistencia[];
+}
+
+/* Actions */
+export class PullPainelRapidoPageState {
+    static readonly type = '[Painel-Rapido-Page] Pulled from: assistencias api';
+}
+
+export class PushPainelRapidoPageState {
+    static readonly type = '[Painel-Rapido-Page] Pushed to: assistencias api';
+}
+
+export class CreatePainelRapidoPageState {
+    static readonly type = '[Painel-Rapido-Page] received "created" from: assistencias api';
+    constructor(public assistencia: Assistencia) { }
+}
+
+export class PatchPainelRapidoPageState {
+    static readonly type = '[Painel-Rapido-Page] received "patched" from: assistencias api';
+    constructor(public assistencia: Assistencia) { }
+}
+/* ###### */
+
+@State<PainelRapidoPageStateModel | null>({
+    name: 'painelRapidoPage',
+    defaults: null
+})
+export class PainelRapidoPageState {
+
+    constructor() {
+    }
+
+}
