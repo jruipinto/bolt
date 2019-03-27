799d95c2a342ea3d0bba9af86120346f104f8d11
J Rui Pinto <rui.jruipinto@gmail.com>
2019-03-27 22:59:59 +0000

    developing painel-rapido-page.state created all actions

--- projecto/frontend3/src/app/pages/dashboard-page/pages/painel-rapido-page/painel-rapido-page.state.ts
+++ projecto/frontend3/src/app/pages/dashboard-page/pages/painel-rapido-page/painel-rapido-page.state.ts
@@ -7,22 +7,62 @@ export interface PainelRapidoPageStateModel {
     pedidosContactoCliente: Assistencia[];
 }
 
-/* Actions */
-export class PullPainelRapidoPageState {
-    static readonly type = '[Painel-Rapido-Page] Pulled from: assistencias api';
+/* Actions for encomendas */
+export class PullPainelRapidoPageEncomendasState {
+    static readonly type = '[Painel-Rapido-Page] Pulled PainelRapidoPageState.encomendas from: Encomendas api';
 }
 
-export class PushPainelRapidoPageState {
-    static readonly type = '[Painel-Rapido-Page] Pushed to: assistencias api';
+export class PushPainelRapidoPageEncomendasState {
+    static readonly type = '[Painel-Rapido-Page] Pushed PainelRapidoPageState.encomendas to: Encomendas api';
 }
 
-export class CreatePainelRapidoPageState {
-    static readonly type = '[Painel-Rapido-Page] received "created" from: assistencias api';
+export class CreatePainelRapidoPageEncomendasState {
+    static readonly type = '[Painel-Rapido-Page] received PainelRapidoPageState.encomendas "created" from: Encomendas api';
+    constructor(public encomenda: Encomenda) { }
+}
+
+export class PatchPainelRapidoPageEncomendasState {
+    static readonly type = '[Painel-Rapido-Page] received PainelRapidoPageState.encomendas "patched" from: Encomendas api';
+    constructor(public encomenda: Encomenda) { }
+}
+/* ###### */
+
+/* Actions for orcamentos */
+export class PullPainelRapidoPageOrcamentosState {
+    static readonly type = '[Painel-Rapido-Page] Pulled PainelRapidoPageState.orcamentos from: Assistencias api';
+}
+
+export class PushPainelRapidoPageOrcamentosState {
+    static readonly type = '[Painel-Rapido-Page] Pushed PainelRapidoPageState.orcamentos to: Assistencias api';
+}
+
+export class CreatePainelRapidoPageOrcamentosState {
+    static readonly type = '[Painel-Rapido-Page] received PainelRapidoPageState.orcamentos "created" from: Assistencias api';
     constructor(public assistencia: Assistencia) { }
 }
 
-export class PatchPainelRapidoPageState {
-    static readonly type = '[Painel-Rapido-Page] received "patched" from: assistencias api';
+export class PatchPainelRapidoPageOrcamentosState {
+    static readonly type = '[Painel-Rapido-Page] received PainelRapidoPageState.orcamentos"patched" from: Assistencias api';
+    constructor(public assistencia: Assistencia) { }
+}
+/* ###### */
+
+/* Actions for pedidosContactoCliente */
+export class PullPainelRapidoPagePedidosContactoClienteState {
+    static readonly type = '[Painel-Rapido-Page] Pulled PainelRapidoPageState.pedidosContactoCliente from: Assistencias api';
+}
+
+export class PushPainelRapidoPagePedidosContactoClienteState {
+    static readonly type = '[Painel-Rapido-Page] Pushed PainelRapidoPageState.pedidosContactoCliente to: Assistencias api';
+}
+
+export class CreatePainelRapidoPagePedidosContactoClienteState {
+    static readonly type = '[Painel-Rapido-Page] received PainelRapidoPageState.pedidosContactoCliente "created" from: Assistencias api';
+    constructor(public assistencia: Assistencia) { }
+}
+
+export class PatchPainelRapidoPagePedidosContactoClienteState {
+    static readonly type = '[Painel-Rapido-Page] received PainelRapidoPageState.pedidosContactoCliente "patched" from: Assistencias api';
     constructor(public assistencia: Assistencia) { }
 }
 /* ###### */
