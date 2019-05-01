301d70cab3f7d2d89f62c13f3f30a4e58efb10b8
J Rui Pinto <rui.jruipinto@gmail.com>
2019-05-01 09:47:37 +0100

    fixed bugs on assistencias-saida-print + fixed clarity design bugs

--- projecto/frontend3/src/app/pages/dashboard-page/modals/assistencia-modal/assistencia-modal.component.ts
+++ projecto/frontend3/src/app/pages/dashboard-page/modals/assistencia-modal/assistencia-modal.component.ts
@@ -3,9 +3,12 @@ import { Observable } from 'rxjs';
 
 import { Select, Store } from '@ngxs/store';
 
-import { AssistenciaModalState, AssistenciaModalStateModel, AssistenciaModalClose,
-  AssistenciaModalPostAssistencia } from './assistencia-modal.state';
+import {
+  AssistenciaModalState, AssistenciaModalStateModel, AssistenciaModalClose,
+  AssistenciaModalPostAssistencia
+} from './assistencia-modal.state';
 import { Assistencia } from 'src/app/shared/models';
+import { PrintService } from 'src/app/pages/dashboard-page/prints/print.service';
 
 
 @Component({
@@ -17,7 +20,7 @@ export class AssistenciaModalComponent implements OnInit {
   @Select(AssistenciaModalState)
   public modalState$: Observable<AssistenciaModalStateModel>;
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private printService: PrintService) {
   }
 
   ngOnInit() {
@@ -27,12 +30,15 @@ export class AssistenciaModalComponent implements OnInit {
     if (modalIsOpen) { this.store.dispatch(new AssistenciaModalClose()); }
   }
 
-  saveModal({ newEstado, assistencia }) {    
-    if (newEstado === "entregue" && assistencia.estado !== "concluído") {
-      alert("Primeiro tens de concluir a assistencia!");
+  saveModal(newEstado: string, assistencia: Assistencia) {
+    if (newEstado === 'entregue' && assistencia.estado !== 'concluído') {
+      alert('Primeiro tens de concluir a assistencia!');
     } else {
-      this.store.dispatch(new AssistenciaModalPostAssistencia({ ...assistencia, estado: newEstado}));
-    }    
+      this.store.dispatch(new AssistenciaModalPostAssistencia({ ...assistencia, estado: newEstado }))
+        .subscribe(() => {
+          if (newEstado === 'entregue') { this.printService.printAssistenciaSaida(assistencia); }
+        });
+    }
   }
 
 }
