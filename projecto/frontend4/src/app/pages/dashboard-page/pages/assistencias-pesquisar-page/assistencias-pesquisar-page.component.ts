1c683a5cff3938d7c9e40c0ff0d9215642da5706
jruipinto <44852805+jruipinto@users.noreply.github.com>
2019-05-27 14:20:18 +0100

    developing assistencias-pesquisar

--- projecto/frontend4/src/app/pages/dashboard-page/pages/assistencias-pesquisar-page/assistencias-pesquisar-page.component.ts
+++ projecto/frontend4/src/app/pages/dashboard-page/pages/assistencias-pesquisar-page/assistencias-pesquisar-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap, first } from 'rxjs/operators';
+import { tap, first, map } from 'rxjs/operators';
 import { AssistenciasService, UIService, UI, UsersService } from 'src/app/shared/state';
-import { Assistencia } from 'src/app/shared';
+import { Assistencia, User } from 'src/app/shared';
 
 export interface Query {
   column: string;
-  condition: string;
+  condition: string | number;
 }
 
 @Component({
@@ -54,63 +54,107 @@ export class AssistenciasPesquisarPageComponent implements OnInit {
       )
       .subscribe();
   }
-
-  addFilter(newSearchFilter: Query) {
-    if (!this.selectedOption) {
-      alert('Tens de decidir o que queres procurar primeiro!');
-      return;
-    }
-    this.searchFilters
-      ? this.searchFilters = [...this.searchFilters, newSearchFilter]
-      : this.searchFilters = [newSearchFilter];
-    this.input = null;
-    this.selectedOption = null;
-  }
-
-  removeSearchFilter(i: number) {
-    this.searchFilters.splice(i, 1);
-  }
-
-  search() {
-    let dbQueryParams: {};
-    if (!this.searchFilters) {
+  /*
+    addFilter(newSearchFilter: Query) {
       if (!this.selectedOption) {
         alert('Tens de decidir o que queres procurar primeiro!');
         return;
       }
-      this.searchFilters = [{ column: this.selectedOption, condition: this.input }];
+      this.searchFilters
+        ? this.searchFilters = [...this.searchFilters, newSearchFilter]
+        : this.searchFilters = [newSearchFilter];
+      this.input = null;
+      this.selectedOption = null;
     }
+    */
 
-    // search client_id here!
-
-    this.searchFilters.forEach((searchFilter: Query) => {
-      const newdbQueryParam = JSON.parse('{"' + searchFilter.column + '" : { "$like" : "%' + searchFilter.condition + '%"} }');
-      dbQueryParams = { ...dbQueryParams, ...newdbQueryParam };
-    });
-    const dbQuery = { query: { $limit: 200, ...dbQueryParams } };
-    this.results$ = this.assistencias
-      .findAndWatch(dbQuery);
-    this.input = null;
-    this.selectedOption = null;
-    this.searchFilters = null;
+  private patchSearchFilters(newSearchFilterToAdd: Query, searchFiltersToPatch: Query[]): Query[] {
+    if (searchFiltersToPatch) {
+      searchFiltersToPatch
+        .splice(
+          searchFiltersToPatch
+            .findIndex((searchFilter: Query) => searchFilter.column === newSearchFilterToAdd.column)
+        );
+      return searchFiltersToPatch = [...searchFiltersToPatch, newSearchFilterToAdd];
+    } else {
+      return searchFiltersToPatch = [newSearchFilterToAdd];
+    }
+  }
+  private findClienteIdByNameOrContactAndPatchSearchFilters(newSearchFilter: Query): Observable<User> {
+    const column = (a) => {
+      a.column === 'cliente_user_name'
+        ? 'nome'
+      : 'contacto'
+  }
+    return this.users
+  .find({
+    query: {
+      $limit: 200,
+      ...JSON.parse('{"' + newSearchFilter.column + '" : { "$like" : "%' + newSearchFilter.condition + '%"} }')
+    }
+  })
+  .pipe(
+    map((res: User[]) => res[0]),
+    tap((user: User) => {
+      const searchFilter: Query = {
+        column: 'cliente_user_id',
+        condition: user.id
+      };
+      this.searchFilters = this.patchSearchFilters(searchFilter, this.searchFilters);
+    })
+  );
   }
 
-  private findClientUserIDbyContacto (searchFilters: Query[]) {
-    const userContactoIndex = searchFilters
-      .findIndex((searchFilter: Query) => searchFilter.column === 'cliente_user_contacto');
-    const clienteUserContacto = searchFilters.splice(userContactoIndex);
-    return this.users.find({query: {cliente_user_contacto: { $like: '%' + clienteUserContacto[0].condition + '%'}}});
+addFilter(newSearchFilter: Query) {
+  if (!newSearchFilter.column) {
+    alert('Tens de decidir o que queres procurar primeiro!');
+    return;
   }
 
-  private findClientUserIDbyName (searchFilters: Query[]): Query[] {
-    const userContactoIndex = searchFilters
-      .findIndex((searchFilter: Query) => searchFilter.column === 'cliente_user_contacto');
-    const userNameIndex = searchFilters
-      .findIndex((searchFilter: Query) => searchFilter.column === 'cliente_user_name');
-    const clienteUserContacto = searchFilters.splice(userContactoIndex);
-    const clienteUserName = searchFilters.splice(userNameIndex);
-
-    return searchFilters;
+  switch (newSearchFilter.column) {
+    case 'cliente_user_name':
+      this.findClienteIdByNameOrContactAndPatchSearchFilters(newSearchFilter)
+        .subscribe();
+      break;
+    case 'cliente_user_contacto':
+      this.findClienteIdByNameOrContactAndPatchSearchFilters(newSearchFilter)
+        .subscribe();
+      break;
+    default:
+      this.searchFilters = this.patchSearchFilters(newSearchFilter, this.searchFilters);
+      break;
   }
+
+  this.input = null;
+  this.selectedOption = null;
+}
+
+removeSearchFilter(i: number) {
+  this.searchFilters.splice(i, 1);
+}
+
+search() {
+  let dbQueryParams: {};
+  if (!this.searchFilters) {
+    if (!this.selectedOption) {
+      alert('Tens de decidir o que queres procurar primeiro!');
+      return;
+    }
+    this.searchFilters = [{ column: this.selectedOption, condition: this.input }];
+  }
+
+  // search client_id here!
+
+  this.searchFilters.forEach((searchFilter: Query) => {
+    const newdbQueryParam = JSON.parse('{"' + searchFilter.column + '" : { "$like" : "%' + searchFilter.condition + '%"} }');
+    dbQueryParams = { ...dbQueryParams, ...newdbQueryParam };
+  });
+  const dbQuery = { query: { $limit: 200, ...dbQueryParams } };
+  this.results$ = this.assistencias
+    .findAndWatch(dbQuery);
+  this.input = null;
+  this.selectedOption = null;
+  this.searchFilters = null;
+}
 
 }
