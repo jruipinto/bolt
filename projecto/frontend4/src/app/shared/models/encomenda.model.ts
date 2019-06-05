73191bd6a5e40742494167f2f492de701438e4b8
J Rui Pinto <rui.jruipinto@gmail.com>
2019-06-05 20:36:35 +0100

    fixed indentation of encomenda.model.ts

--- projecto/frontend4/src/app/shared/models/encomenda.model.ts
+++ projecto/frontend4/src/app/shared/models/encomenda.model.ts
@@ -1,12 +1,12 @@
 export interface Encomenda {
-	id: number;
-	artigo_id: number;
-	assistencia_id: number;
-	cliente_user_id: number;
-	observacao: string;
-	estado: string;
-	previsao_entrega: string;
-	orcamento: number;
-	fornecedor: string;
-	qty: number;
+    id: number;
+    artigo_id: number;
+    assistencia_id: number;
+    cliente_user_id: number;
+    observacao: string;
+    estado: string;
+    previsao_entrega: string;
+    orcamento: number;
+    fornecedor: string;
+    qty: number;
 }
