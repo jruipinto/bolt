249f88bd4f952abdc6f60a6ecaf749be6afaca72
jruipinto <44852805+jruipinto@users.noreply.github.com>
2019-06-05 14:24:32 +0100

    updated encomendas-criar-nova-page

--- projecto/frontend4/src/app/pages/dashboard-page/pages/encomendas-criar-nova-page/encomendas-criar-nova-page.component.ts
+++ projecto/frontend4/src/app/pages/dashboard-page/pages/encomendas-criar-nova-page/encomendas-criar-nova-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { EncomendasService } from 'src/app/shared/state';
 
 @Component({
   selector: 'app-encomendas-criar-nova-page',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./encomendas-criar-nova-page.component.scss']
 })
 export class EncomendasCriarNovaPageComponent implements OnInit {
+  public encomendaForm = this.fb.group({
+    id: [null],
+    artigo_id: [null],
+    assistencia_id: [null],
+    cliente_user_id: [null],
+    observacao: [null],
+    estado: [null],
+    previsao_entrega: [null],
+    orcamento: [null],
+    fornecedor: [null],
+    qty: [null]
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private encomendas: EncomendasService
+  ) { }
 
   ngOnInit() {
   }
--- projecto/frontend4/src/app/shared/models/encomenda.model.ts
+++ projecto/frontend4/src/app/shared/models/encomenda.model.ts
@@ -2,7 +2,7 @@ export interface Encomenda{
 	id: number;
 	artigo_id: number;
 	assistencia_id: number;
-	cliente_id: number;
+	cliente_user_id: number;
 	observacao: string;
 	estado: string
 	previsao_entrega: string;
