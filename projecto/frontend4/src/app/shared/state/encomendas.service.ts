5404f668142294966608f23d6046a55830b3fda8
J Rui Pinto <rui.jruipinto@gmail.com>
2019-06-09 13:41:23 +0100

    updated encomendas.service to implement registo cronologico on every encomenda

--- projecto/frontend4/src/app/shared/state/encomendas.service.ts
+++ projecto/frontend4/src/app/shared/state/encomendas.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@angular/core';
-import { EncomendasApiService } from 'src/app/shared/services';
+import { EncomendasApiService, AuthService } from 'src/app/shared/services';
 import { EntityStateAbstraction } from 'src/app/shared/abstraction-classes';
+import { Encomenda, EventoCronologico } from '..';
 
 @Injectable({ providedIn: 'root' })
 export class EncomendasService extends EntityStateAbstraction {
 
-  constructor(protected encomendasAPI: EncomendasApiService) {
-      super(encomendasAPI);
-    }
+  constructor(
+    protected encomendasAPI: EncomendasApiService,
+    private authService: AuthService) {
+    super(encomendasAPI);
+  }
 
+  public create(encomenda: Partial<Encomenda>) {
+    const registo_cronologico: EventoCronologico[] = [{
+      tecnico_user_id: this.authService.getUserId(),
+      estado: encomenda.estado,
+      updatedAt: new Date().toLocaleString() // dia/mes/ano, hora:minuto:segundo naquele momento
+    }];
+    return super.create({ ...encomenda, registo_cronologico } as Encomenda);
+  }
+
+  public patch(id: number, encomenda: Partial<Encomenda>) {
+    const novoRegisto: EventoCronologico = {
+      tecnico_user_id: this.authService.getUserId(),
+      estado: encomenda.estado,
+      updatedAt: new Date().toLocaleString()
+    };
+    const updatedRegistoCronologico: EventoCronologico[] = [
+      ...encomenda.registo_cronologico,
+      novoRegisto
+    ];
+    return super.patch(
+      id,
+      { ...encomenda, registo_cronologico: updatedRegistoCronologico } as Encomenda
+    );
+  }
 }
