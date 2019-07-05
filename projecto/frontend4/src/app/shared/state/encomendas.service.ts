903ba579cce2f982a6f1d2a278aea4ca5526740a
J Rui Pinto <rui.jruipinto@gmail.com>
2019-07-05 23:42:01 +0100

    updated encomendas.service

--- projecto/frontend4/src/app/shared/state/encomendas.service.ts
+++ projecto/frontend4/src/app/shared/state/encomendas.service.ts
@@ -1,15 +1,80 @@
 import { Injectable } from '@angular/core';
 import { EncomendasApiService, AuthService } from 'src/app/shared/services';
 import { EntityStateAbstraction } from 'src/app/shared/abstraction-classes';
-import { Encomenda, EventoCronologico } from '..';
+import { Artigo, Encomenda, EventoCronologico } from '..';
+import { map, concatMap, mergeMap } from 'rxjs/operators';
+import { concat } from 'rxjs';
+import { ArtigosService } from './artigos.service';
+import { UsersService } from './users.service';
+import { User } from '../models';
 
 @Injectable({ providedIn: 'root' })
 export class EncomendasService extends EntityStateAbstraction {
 
   constructor(
     protected encomendasAPI: EncomendasApiService,
-    private authService: AuthService) {
+    private authService: AuthService,
+    private artigosService: ArtigosService,
+    private usersService: UsersService) {
     super(encomendasAPI);
+  }
+
+  public get(id: number) {
+    return super.get(id)
+      .pipe(
+        map((res: Encomenda[]) => res[0]),
+        concatMap(encomenda => this.artigosService.get(encomenda.artigo_id)
+          .pipe(
+            map((dbArtigo: Artigo[]) => dbArtigo[0]),
+            map(dbArtigo => ({
+              ...encomenda,
+              artigo_marca: dbArtigo.marca,
+              artigo_modelo: dbArtigo.modelo,
+              artigo_descricao: dbArtigo.descricao,
+            }))
+          )
+        ),
+        concatMap(encomenda => this.usersService.get(encomenda.cliente_user_id)
+          .pipe(
+            map((dbUser: User[]) => dbUser[0]),
+            map(dbUser => ({
+              ...encomenda,
+              cliente_user_name: dbUser.nome,
+              cliente_user_contacto: dbUser.contacto
+            }))
+          )
+        ),
+        map(res => [res])
+      );
+  }
+
+  public getAndWatch(id: number) {
+    return super.getAndWatch(id)
+      .pipe(
+        map((res: Encomenda[]) => res[0]),
+        mergeMap(encomenda => this.artigosService.get(encomenda.artigo_id)
+          .pipe(
+            map((dbArtigo: Artigo[]) => dbArtigo[0]),
+            map(dbArtigo => ({
+              ...encomenda,
+              artigo_marca: dbArtigo.marca,
+              artigo_modelo: dbArtigo.modelo,
+              artigo_descricao: dbArtigo.descricao,
+            }))
+          )
+        ),
+        mergeMap(encomenda => this.usersService.get(encomenda.cliente_user_id)
+          .pipe(
+            map((dbUser: User[]) => dbUser[0]),
+            map(dbUser => ({
+              ...encomenda,
+              cliente_user_name: dbUser.nome,
+              cliente_user_contacto: dbUser.contacto
+            }))
+          )
+        ),
+        map(res => [res])
+      );
   }
 
   public create(encomenda: Partial<Encomenda>) {
