076faf0c26be8844556e6aed3b1f614e6e464812
jruipinto <44852805+jruipinto@users.noreply.github.com>
2019-07-30 14:09:35 +0100

    updated UX on encomendas-pesquisar-page (implementing new search UX on encomendas-pesquisar in progress)

--- projecto/frontend4/src/app/pages/dashboard-page/pages/encomendas-pesquisar-page/encomendas-pesquisar-page.component.ts
+++ projecto/frontend4/src/app/pages/dashboard-page/pages/encomendas-pesquisar-page/encomendas-pesquisar-page.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
-import { Observable } from 'rxjs';
-import { Encomenda, Query, User } from 'src/app/shared';
-import { EncomendasService, UsersService, UIService } from 'src/app/shared/state';
-import { map, tap } from 'rxjs/operators';
+import { Observable, concat } from 'rxjs';
+import { Encomenda, Query, User, Artigo } from 'src/app/shared';
+import { EncomendasService, UsersService, UIService, ArtigosService } from 'src/app/shared/state';
+import { map, tap, concatMap, toArray } from 'rxjs/operators';
+import { clone } from 'ramda';
 
 @AutoUnsubscribe()
 @Component({
@@ -14,49 +15,25 @@ import { map, tap } from 'rxjs/operators';
   styleUrls: ['./encomendas-pesquisar-page.component.scss']
 })
 export class EncomendasPesquisarPageComponent implements OnInit, OnDestroy {
-  results$: Observable<Encomenda[]>;
+  public userSearchModal = false;
+  public userSearchResults$: Observable<User[]>;
+  public results$: Observable<Encomenda[]>;
+  public encomendasSearchForm = this.fb.group({
+    input: [''],
+    estado: ['qualquer'],
+    cliente: ['']
+  });
 
-  public selectedOption: string;
-  public input: string;
-  public searchFilters: Query[];
-
-  /*
-  {
-    id: number;
-    artigo_id: number;
-    artigo_marca?: string;
-    artigo_modelo?: string;
-    artigo_descricao?: string;
-    assistencia_id: number;
-    cliente_user_id: number;
-    cliente_user_name?: string;
-    cliente_user_contacto?: number;
-    observacao: string;
-    registo_cronologico: EventoCronologico[]; // JSON.stringify(data: EventoCronologico[])
-    estado: string;
-    previsao_entrega: string;
-    orcamento: number;
-    fornecedor: string;
-    qty: number;
-}
-  */
-
-  public filterOptions = [
-    'id',
-    'assistencia_id',
-    'artigo_id',
-    'cliente_user_id',
-    'cliente_user_name',
-    'cliente_user_contacto',
-    'fornecedor',
-    'estado',
-    // 'createdAt'
-  ];
+  public userSearchForm = this.fb.group({
+    input: ['']
+  });
 
   constructor(
     private encomendas: EncomendasService,
+    private artigos: ArtigosService,
     private users: UsersService,
-    private router: Router) { }
+    private router: Router,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
   }
@@ -68,94 +45,99 @@ export class EncomendasPesquisarPageComponent implements OnInit, OnDestroy {
     return this.router.navigate(['/dashboard/encomenda', encomendaID]);
   }
 
-  private patchSearchFilters(newSearchFilterToAdd: Query, searchFiltersToPatch: Query[]): Query[] {
-    if (searchFiltersToPatch) {
-      const i = searchFiltersToPatch
-        .findIndex((searchFilter: Query) => searchFilter.column === newSearchFilterToAdd.column);
-      if (i > -1) { searchFiltersToPatch.splice(i, 1); }
-      return [...searchFiltersToPatch, newSearchFilterToAdd];
-    } else {
-      return [newSearchFilterToAdd];
-    }
-  }
-  private findClienteIdByNameOrContactAndPatchSearchFilters(newSearchFilter: Query): Observable<User> {
-    const column = (a) => {
-      if (a === 'cliente_user_name') {
-        return 'nome';
-      } else {
-        return 'contacto';
-      }
-    };
-    return this.users
-      .find({
-        query: {
-          $limit: 200,
-          ...JSON.parse('{"' + column(newSearchFilter.column) + '" : { "$like" : "%' + newSearchFilter.condition + '%"} }')
-        }
-      })
-      .pipe(
-        map((res: User[]) => res[0]),
-        tap((user: User) => {
-          const searchFilter: Query = {
-            column: 'cliente_user_id',
-            condition: user.id
-          };
-          this.searchFilters = this.patchSearchFilters(searchFilter, this.searchFilters);
-        })
+  searchUser(input: string) {
+    if (input) {
+      const inputSplited = input.split(' ');
+      const inputMapped = inputSplited.map(word =>
+        '{"$or": [' +
+        '{ "nome": { "$like": "%' + word + '%" }},' +
+        '{ "contacto": { "$like": "%' + word + '%" }}' +
+        ' ]}'
       );
+      const dbQuery =
+        '{' +
+        '"query": {' +
+        '"$limit": "200",' +
+        '"$and": [' +
+        inputMapped +
+        ']' +
+        '}' +
+        '}';
+
+      this.userSearchResults$ = this.users
+        .find(JSON.parse(dbQuery));
+    }
   }
 
-  addFilter(newSearchFilter: Query) {
-    if (!newSearchFilter.column) {
-      alert('Tens de decidir o que queres procurar primeiro!');
-      return;
-    }
-
-    switch (newSearchFilter.column) {
-      case 'cliente_user_name':
-        this.findClienteIdByNameOrContactAndPatchSearchFilters(newSearchFilter)
-          .subscribe();
-        break;
-      case 'cliente_user_contacto':
-        this.findClienteIdByNameOrContactAndPatchSearchFilters(newSearchFilter)
-          .subscribe();
-        break;
-      default:
-        this.searchFilters = this.patchSearchFilters(newSearchFilter, this.searchFilters);
-        break;
-    }
-
-    this.input = null;
-    this.selectedOption = null;
+  addUser(user: User) {
+    this.encomendasSearchForm.patchValue({ cliente: clone(user.id) });
+    this.userSearchModal = false;
   }
 
-  removeSearchFilter(i: number) {
-    this.searchFilters.splice(i, 1);
-  }
+  /*searchEncomenda(input: string, estado?: string, cliente?: number) {
+    const inputSplited = input.split(' ');
+    const inputMapped = inputSplited.map(word =>
+      '{"$or": [' +
+      '{ "categoria": { "$like": "%' + word + '%" }},' +
+      '{ "marca": { "$like": "%' + word + '%" }},' +
+      '{ "modelo": { "$like": "%' + word + '%" }},' +
+      '{ "cor": { "$like": "%' + word + '%" }},' +
+      '{ "serial": { "$like": "%' + word + '%" }},' +
+      '{ "problema": { "$like": "%' + word + '%" }}' +
+      ' ]}'
+    );
 
-  search() {
-    let dbQueryParams: {};
-    if (!this.searchFilters) {
-      if (!this.selectedOption) {
-        alert('Tens de decidir o que queres procurar primeiro!');
-        return;
-      }
-      // this.searchFilters = [{ column: this.selectedOption, condition: this.input }];
-      this.addFilter({ column: this.selectedOption, condition: this.input });
-    }
+    const clienteStatement = cliente && typeof cliente === 'number' ? ',"cliente_user_id":' + cliente : '';
+    const estadoStatement = estado && estado !== 'qualquer' ? ',"estado": "' + estado + '"' : '';
+    const dbQuery =
+      '{' +
+      '"query": {' +
+      '"$limit": "200",' +
+      '"$and": [' +
+      inputMapped +
+      ']' +
+      estadoStatement +
+      clienteStatement +
+      '}' +
+      '}';
 
-    // search client_id here!
-
-    this.searchFilters.forEach((searchFilter: Query) => {
-      const newdbQueryParam = JSON.parse('{"' + searchFilter.column + '" : { "$like" : "%' + searchFilter.condition + '%"} }');
-      dbQueryParams = { ...dbQueryParams, ...newdbQueryParam };
-    });
-    const dbQuery = { query: { $limit: 200, ...dbQueryParams } };
     this.results$ = this.encomendas
-      .findAndWatch(dbQuery);
-    this.input = null;
-    this.selectedOption = null;
-    this.searchFilters = null;
+      .find(JSON.parse(dbQuery));
+  }*/
+
+  searchEncomenda(input: string, estado?: string, cliente?: number) {
+    const inputSplited = input.split(' ');
+    const inputMapped = inputSplited.map(word =>
+      '{"$or": [' +
+      '{ "marca": { "$like": "%' + word + '%" }},' +
+      '{ "modelo": { "$like": "%' + word + '%" }},' +
+      '{ "descricao": { "$like": "%' + word + '%" }}' +
+      ' ]}'
+    );
+
+    const dbQuery =
+      '{' +
+      '"query": {' +
+      '"$limit": "200",' +
+      '"$and": [' +
+      inputMapped +
+      ']' +
+      '}' +
+      '}';
+
+    this.results$ = this.artigos
+      .find(JSON.parse(dbQuery))
+      .pipe(
+        concatMap(
+          (artigosDB: Artigo[]) => concat(artigosDB.map(
+            ({ id }) => this.encomendas.find({ query: { artigo_id: id } })
+              .pipe(map(res => res[0]))
+          )).pipe(concatMap(concats => concats), toArray())
+        )
+      );
+
+    // this.results$ = this.encomendas.find(JSON.parse(dbQuery));
   }
+
 
 }
