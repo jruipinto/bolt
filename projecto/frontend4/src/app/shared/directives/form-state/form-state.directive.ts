d2420f31bfb44a2cdf5a829c6765d6ec7067b47b
J Rui Pinto <rui.jruipinto@gmail.com>
2019-11-01 22:47:44 +0000

    refact: finished FormState directive

--- projecto/frontend4/src/app/shared/directives/form-state/form-state.directive.ts
+++ projecto/frontend4/src/app/shared/directives/form-state/form-state.directive.ts
@@ -1,12 +1,13 @@
-import { Directive, OnChanges, Input } from '@angular/core';
-import { UIService } from '../../state';
+import { Directive, Input, OnInit } from '@angular/core';
+import { UIService, UI } from '../../state';
 import { FormGroupDirective } from '@angular/forms';
+import { first, concatMap, tap } from 'rxjs/operators';
 
 @Directive({
   // tslint:disable-next-line: directive-selector
   selector: '[formState]'
 })
-export class FormStateDirective implements OnChanges {
+export class FormStateDirective implements OnInit {
 
   @Input('formState')
   // tslint:disable-next-line: no-non-null-assertion
@@ -17,9 +18,22 @@ export class FormStateDirective implements OnChanges {
     private formGroupDirective: FormGroupDirective
   ) { }
 
-  ngOnChanges() {
+  ngOnInit() {
     const form = this.formGroupDirective.control;
-    console.log('formulario:', form.value, this.path);
+
+    this.uiService.state$
+      .pipe(
+        first(),
+        tap((uiState: UI) => form.patchValue(uiState[this.path]))
+      ).subscribe();
+
+    form.valueChanges
+      .pipe(
+        concatMap(
+          (formValue) => this.uiService.patchState({ [this.path]: formValue })
+        )
+      )
+      .subscribe();
   }
 
 }
