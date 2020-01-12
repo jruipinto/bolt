dbf0ef75285c88916e5bc14bc10bddaad931e77f
J Rui Pinto <rui.jruipinto@gmail.com>
2020-01-12 18:29:08 +0000

    refactor: add error handling for SMS sending

--- projecto/backend/src/hooks/send-by-sms.js
+++ projecto/backend/src/hooks/send-by-sms.js
@@ -1,14 +1,42 @@
 const modem = require('../modem.js');
 
 const send = function (context) {
-  const phoneNumber = context.data.phoneNumber || null;
-  const text = context.data.text || null;
+    const phoneNumber = context.data.phoneNumber || null;
+    const text = context.data.text || null;
 
-  modem.sendSMS({ phoneNumber, text })
-    .subscribe(report => {
-      console.log('Message delivered! Here is the report:', report);
-      context.app.service(context.path).patch(+context.result.id, { state: "gsyss", deliveredAt: report.deliveryTime });
-    });
+    if (modem.status.error || !modem.status.connected) {
+        console.log('Modem error: Verify modem. Status:', modem.status);
+        context.app.service(context.path).patch(+context.result.id, { state: "modem error" });
+        return;
+    }
+
+    modem.sendSMS({ phoneNumber, text })
+        .subscribe(
+            report => {
+                if (+report.st === 0) {
+                    context.app.service(context.path).patch(
+                        +context.result.id,
+                        {
+                            state: "delivered",
+                            submitedAt: report.submitTime,
+                            deliveredAt: report.deliveryTime
+                        }
+                    );
+                } else {
+                    context.app.service(context.path).patch(
+                        +context.result.id,
+                        {
+                            state: "unreachable",
+                            submitedAt: report.submitTime
+                        }
+                    );
+                }
+            },
+            err => {
+                console.log('SMS error:', err);
+                context.app.service(context.path).patch(+context.result.id, { state: "modem error" });
+            }
+        );
 };
 
 module.exports = send;
