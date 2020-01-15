35a288c843d139980b5439bcca46a2568db24045
J Rui Pinto <rui.jruipinto@gmail.com>
2020-01-15 00:03:29 +0000

    feat: implements modem's logs saving in DB

--- projecto/backend/src/modem.js
+++ projecto/backend/src/modem.js
@@ -1,3 +1,9 @@
+// import { from } from 'rxjs';
+const from = require('rxjs').from;
+// import { concatMap, tap, map } from 'rxjs/operators';
+const concatMap = require('rxjs/operators').concatMap;
+const tap = require('rxjs/operators').tap;
+const map = require('rxjs/operators').map;
 const Modem = require('modemjs').Modem;
 let modemConfig;
 
@@ -24,6 +30,20 @@ const listen = function (app) {
             }
         );
     });
+
+    let newLog = null;
+    modem.log$.pipe(
+        map(log => log.trim()),
+        tap(log => newLog = (newLog ? (newLog + ';\r' + log) : log).slice(-1500)),
+        concatMap(() => from(
+            app.service('configs').patch(
+                null,
+                { value: newLog },
+                { query: { key: "modemLog" } }
+            )
+        ))
+    ).subscribe();
+
 }
 
 module.exports.modem = modem;
