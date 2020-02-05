42cb5a2fc04af3d226c6018eb0686c607f17fe8a
J Rui Pinto <rui.jruipinto@gmail.com>
2020-02-05 09:19:42 +0000

    refactor: add special character conversion table

--- projecto/frontend4/src/app/shared/services/messages-api.service.ts
+++ projecto/frontend4/src/app/shared/services/messages-api.service.ts
@@ -21,7 +21,7 @@ export class MessagesApiService extends EntityApiAbstration {
         return this.create({
             phoneNumber: assistencia.cliente_user_contacto,
             // tslint:disable-next-line: max-line-length
-            text: `Bom dia,\n\nN Reparacoes informa que o orcamento da assistencia ${assistencia.id} e de ${assistencia.preco} euros.\n\nObrigado.`,
+            text: `Bom dia,\n\nN Reparações informa que o orcamento da assistência ${assistencia.id} é de ${assistencia.preco} €.\n\nObrigado.`,
             tecnico_user_id: this.authService.getUserId(),
             state: 'pending',
             subject: `Orçamento de A${assistencia.id}`
@@ -93,3 +93,161 @@ export class MessagesApiService extends EntityApiAbstration {
     }
 
 }
+
+const specialCharactersConversionTable = [
+    {
+        specialCharacter: '@',
+        replacement: '\x00'
+    },
+    {
+        specialCharacter: '%',
+        replacement: '\x25'
+    },
+    {
+        specialCharacter: '€',
+        replacement: 'Eur'
+    },
+    {
+        specialCharacter: '$',
+        replacement: '\x02'
+    },
+    {
+        specialCharacter: 'ç',
+        replacement: 'c'
+    },
+    {
+        specialCharacter: 'à',
+        replacement: '\x7f'
+    },
+    {
+        specialCharacter: 'á',
+        replacement: 'a'
+    },
+    {
+        specialCharacter: 'ã',
+        replacement: 'a'
+    },
+    {
+        specialCharacter: 'â',
+        replacement: 'a'
+    },
+    {
+        specialCharacter: 'è',
+        replacement: '\x04'
+    },
+    {
+        specialCharacter: 'é',
+        replacement: '\x05'
+    },
+    {
+        specialCharacter: 'ê',
+        replacement: 'e'
+    },
+    {
+        specialCharacter: 'ì',
+        replacement: 'i'
+    },
+    {
+        specialCharacter: 'í',
+        replacement: 'i'
+    },
+    {
+        specialCharacter: 'ò',
+        replacement: '\x08'
+    },
+    {
+        specialCharacter: 'ó',
+        replacement: 'o'
+    },
+    {
+        specialCharacter: 'õ',
+        replacement: 'o'
+    },
+    {
+        specialCharacter: 'ô',
+        replacement: 'o'
+    },
+    {
+        specialCharacter: 'ù',
+        replacement: '\x06'
+    },
+    {
+        specialCharacter: 'ú',
+        replacement: 'u'
+    },
+    {
+        specialCharacter: 'û',
+        replacement: 'u'
+    },
+
+
+
+    {
+        specialCharacter: 'Ç',
+        replacement: 'C'
+    },
+    {
+        specialCharacter: 'À',
+        replacement: '\x7f'
+    },
+    {
+        specialCharacter: 'Á',
+        replacement: 'A'
+    },
+    {
+        specialCharacter: 'Ã',
+        replacement: 'A'
+    },
+    {
+        specialCharacter: 'Â',
+        replacement: 'A'
+    },
+    {
+        specialCharacter: 'È',
+        replacement: '\x04'
+    },
+    {
+        specialCharacter: 'É',
+        replacement: '\x1f'
+    },
+    {
+        specialCharacter: 'Ê',
+        replacement: 'E'
+    },
+    {
+        specialCharacter: 'Ì',
+        replacement: 'I'
+    },
+    {
+        specialCharacter: 'Í',
+        replacement: 'I'
+    },
+    {
+        specialCharacter: 'Ò',
+        replacement: '\x08'
+    },
+    {
+        specialCharacter: 'Ó',
+        replacement: 'O'
+    },
+    {
+        specialCharacter: 'Õ',
+        replacement: 'O'
+    },
+    {
+        specialCharacter: 'Ô',
+        replacement: 'O'
+    },
+    {
+        specialCharacter: 'Ù',
+        replacement: '\x06'
+    },
+    {
+        specialCharacter: 'Ú',
+        replacement: 'U'
+    },
+    {
+        specialCharacter: 'Û',
+        replacement: 'U'
+    },
+];
