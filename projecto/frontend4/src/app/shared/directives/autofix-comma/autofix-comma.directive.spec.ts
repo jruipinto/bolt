f8e9356d5ae0d2e9ade0b02a41c3a68707121fea
J Rui Pinto <rui.jruipinto@gmail.com>
2020-02-06 21:46:49 +0000

    refactor: delete unusefull autofix-comma directive

--- projecto/frontend4/src/app/shared/directives/autofix-comma/autofix-comma.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutofixCommaDirective } from './autofix-comma.directive';
-
-describe('AutofixCommaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutofixCommaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- projecto/frontend4/src/app/shared/directives/autofix-comma/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './autofix-comma.module';
