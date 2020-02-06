e08e5c0737ff391adee66e7620b11540019dba9e
jruipinto <44852805+jruipinto@users.noreply.github.com>
2020-02-06 13:35:25 +0000

    refactor: comment  out messages service to avoid mistakes

--- projecto/frontend4/src/app/shared/state/messages.service.ts
+++ projecto/frontend4/src/app/shared/state/messages.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
-import { EntityStateAbstraction } from 'src/app/shared/abstraction-classes';
-import { MessagesApiService } from 'src/app/shared/services/messages-api.service';
+/** Not implemented yet for project schedule and simplicity reasons */
 
-@Injectable({ providedIn: 'root' })
-export class MessagesService extends EntityStateAbstraction {
 
-    constructor(protected messagesAPI: MessagesApiService) {
-        super(messagesAPI);
-    }
+// import { Injectable } from '@angular/core';
+// import { EntityStateAbstraction } from 'src/app/shared/abstraction-classes';
+// import { MessagesApiService } from 'src/app/shared/services/messages-api.service';
 
-}
+// @Injectable({ providedIn: 'root' })
+// export class MessagesService extends EntityStateAbstraction {
+
+//     constructor(protected messagesAPI: MessagesApiService) {
+//         super(messagesAPI);
+//     }
+
+// }
