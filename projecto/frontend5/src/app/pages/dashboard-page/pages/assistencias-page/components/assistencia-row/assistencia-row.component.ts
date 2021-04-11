41903f6ca74eb59b3d5dfa88cb439c69cd56bbd5
jruipinto <social.jruipinto@gmail.com>
2021-04-12 00:42:27 +0100

    refactor: improve assistencia-row acessibility

--- projecto/frontend5/src/app/pages/dashboard-page/pages/assistencias-page/components/assistencia-row/assistencia-row.component.ts
+++ projecto/frontend5/src/app/pages/dashboard-page/pages/assistencias-page/components/assistencia-row/assistencia-row.component.ts
@@ -5,6 +5,9 @@ import {
   EventEmitter,
   Input,
   Output,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
 } from '@angular/core';
 import { Assistencia } from 'src/app/shared';
 
@@ -19,10 +22,51 @@ export class AssistenciaRowComponent implements OnInit {
   @Output() runAssistencia = new EventEmitter<number>();
   @Output() openAssistencia = new EventEmitter<number>();
   @Output() assignAssistencia = new EventEmitter<number>();
+  @ViewChild('collapsedView') collapsedViewEl: ElementRef;
+  @ViewChild('expandedView') expandedViewEl: ElementRef;
+  @ViewChild('buttonsContainer') buttonsContainerEl: ElementRef;
 
-  isSelected = false;
+  isExpanded = false;
 
-  constructor() {}
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
+
+  expandView(expand: boolean) {
+    const w =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    if (w < 992) {
+      return;
+    }
+    if (expand) {
+      this.expandedViewEl.nativeElement.style.maxHeight = '1000px';
+      this.expandedViewEl.nativeElement.style.opacity = '1';
+      this.expandedViewEl.nativeElement.style.transition =
+        'max-height 0.25s ease-in, opacity 0.25s ease';
+      this.collapsedViewEl.nativeElement.style.opacity = '0';
+      this.collapsedViewEl.nativeElement.style.transition =
+        'opacity 0.1s linear';
+      setTimeout(() => {
+        this.buttonsContainerEl.nativeElement.style.display = 'block';
+      }, 100);
+      this.isExpanded = expand;
+    } else {
+      this.expandedViewEl.nativeElement.style.maxHeight = '0';
+      this.expandedViewEl.nativeElement.style.opacity = '0';
+      this.expandedViewEl.nativeElement.style.transition =
+        'max-height 0.2s ease-out, opacity 0.3s ease-out';
+      this.collapsedViewEl.nativeElement.style.opacity = '1';
+      this.collapsedViewEl.nativeElement.style.transition =
+        'opacity 0.4s ease-in';
+      setTimeout(() => {
+        this.buttonsContainerEl.nativeElement.style.display = 'none';
+      }, 100);
+      setTimeout(() => {
+        this.isExpanded = expand;
+        this.cdr.detectChanges();
+      }, 300);
+    }
+  }
 }
