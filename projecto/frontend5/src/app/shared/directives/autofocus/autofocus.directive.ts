ca102c0245e1343966488c641e1c637945a965f1
jruipinto <social.jruipinto@gmail.com>
2021-05-03 00:26:59 +0100

    refactor: add cdk's FocusMonitor

--- projecto/frontend5/src/app/shared/directives/autofocus/autofocus.directive.ts
+++ projecto/frontend5/src/app/shared/directives/autofocus/autofocus.directive.ts
@@ -1,15 +1,20 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { Directive, AfterViewInit, ElementRef } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line: directive-selector
-  selector: '[autofocus]'
+  selector: '[autofocus]',
 })
 export class AutofocusDirective implements AfterViewInit {
-
-  constructor(private el: ElementRef) { }
+  constructor(
+    private el: ElementRef<HTMLElement>,
+    private focusMonitor: FocusMonitor
+  ) {}
 
   ngAfterViewInit() {
-    this.el.nativeElement.select();
+    setTimeout(
+      () => this.focusMonitor.focusVia(this.el.nativeElement, 'program'),
+      0.1
+    );
   }
-
 }
