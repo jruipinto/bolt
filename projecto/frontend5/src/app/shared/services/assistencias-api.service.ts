240917fe2e5d011c192bc6054a1067c6ecd93d70
jruipinto <social.jruipinto@gmail.com>
2021-05-05 01:36:43 +0100

    perf(AssistenciasApiService): query less details
    when using find() method, retrieves less columns of the
    assistencia entity because don't query unnecessary (for
    the context of finding) columns, improving find() speed

--- projecto/frontend5/src/app/shared/services/assistencias-api.service.ts
+++ projecto/frontend5/src/app/shared/services/assistencias-api.service.ts
@@ -10,7 +10,7 @@ import { clone } from 'ramda';
 import { deserialize } from 'src/app/shared/utilities';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AssistenciasApiService extends EntityApiAbstration {
   private usersAPI = this.usersService;
@@ -21,8 +21,12 @@ export class AssistenciasApiService extends EntityApiAbstration {
    * [{id, qty}, {id, qty}, ...]
    */
   private simplify(arr: any[] | null): any[] | null {
-    if (!arr) { return arr; }
-    return arr.map(item => (item.qty ? { id: item.id, qty: item.qty } : { id: item.id }));
+    if (!arr) {
+      return arr;
+    }
+    return arr.map((item) =>
+      item.qty ? { id: item.id, qty: item.qty } : { id: item.id }
+    );
   }
 
   /**
@@ -30,103 +34,273 @@ export class AssistenciasApiService extends EntityApiAbstration {
    * deserializing its JSON props and fecthing the props
    * from the their respective entities on API
    */
-  private fullyDetailedAssistencias$ = (assistenciasFromApi$: Observable<Assistencia[]>) =>
+  private fullyDetailedAssistencias$ = (
+    assistenciasFromApi$: Observable<Assistencia[]>
+  ) =>
     assistenciasFromApi$.pipe(
-      concatMap((assistenciasFromApi: Assistencia[]) => (
-        concat(...assistenciasFromApi.map(
-          (assistenciaFromApi) => (this.usersAPI.get(assistenciaFromApi.cliente_user_id).pipe(
+      concatMap((assistenciasFromApi: Assistencia[]) =>
+        concat(
+          ...assistenciasFromApi.map((assistenciaFromApi) =>
+            this.usersAPI.get(assistenciaFromApi.cliente_user_id).pipe(
+              // retrieve client data
+              map((res: User[]) => res[0]),
+              map(
+                (cliente): Assistencia => ({
+                  ...assistenciaFromApi,
+                  cliente_user_name: cliente.nome,
+                  cliente_user_contacto: cliente.contacto,
+                })
+              ),
 
-            // retrieve client data
-            map((res: User[]) => res[0]),
-            map((cliente): Assistencia => ({
-              ...assistenciaFromApi,
-              cliente_user_name: cliente.nome,
-              cliente_user_contacto: cliente.contacto
-            })),
+              // deserialize JSON props
+              map((assistencia) =>
+                deserialize(assistencia, 'registo_cronologico')
+              ),
+              map((assistencia) => deserialize(assistencia, 'material')),
+              map((assistencia) => deserialize(assistencia, 'encomendas')),
+              map((assistencia) => deserialize(assistencia, 'messages')),
 
-            // deserialize JSON props
-            map(assistencia => deserialize(assistencia, 'registo_cronologico')),
-            map(assistencia => deserialize(assistencia, 'material')),
-            map(assistencia => deserialize(assistencia, 'encomendas')),
-            map(assistencia => deserialize(assistencia, 'messages')),
+              // retrieve tecnico data for all eventos of registo_cronologico
+              concatMap((assistencia) =>
+                concat(
+                  ...assistencia.registo_cronologico.map((evento) => {
+                    if (evento.tecnico_user_id) {
+                      return this.usersAPI.get(evento.tecnico_user_id).pipe(
+                        map(
+                          (user: User[]): EventoCronologico => ({
+                            ...evento,
+                            tecnico: user[0].nome,
+                          })
+                        )
+                      );
+                    }
+                    return of(evento);
+                  })
+                ).pipe(
+                  toArray(),
+                  map(
+                    (registo_cronologico): Assistencia => ({
+                      ...assistencia,
+                      registo_cronologico,
+                    })
+                  )
+                )
+              ),
 
-            // retrieve tecnico data for all eventos of registo_cronologico
-            concatMap(assistencia => (
-              concat(...assistencia.registo_cronologico.map(evento => {
-                if (evento.tecnico_user_id) {
-                  return this.usersAPI.get(evento.tecnico_user_id).pipe(
-                    map((user: User[]): EventoCronologico => ({ ...evento, tecnico: user[0].nome }))
+              // retrieve editor data for all eventos of registo_cronologico
+              concatMap((assistencia) =>
+                concat(
+                  ...assistencia.registo_cronologico.map((evento) => {
+                    if (evento.editor_user_id) {
+                      // check because of backwards compatibility
+                      return this.usersAPI.get(evento.editor_user_id).pipe(
+                        map(
+                          (user: User[]): EventoCronologico => ({
+                            ...evento,
+                            editor: user[0].nome,
+                          })
+                        )
+                      );
+                    }
+                    return of(evento);
+                  })
+                ).pipe(
+                  toArray(),
+                  map(
+                    (registo_cronologico): Assistencia => ({
+                      ...assistencia,
+                      registo_cronologico,
+                    })
+                  )
+                )
+              ),
+
+              // get assistencia.tecnico from api or from the last evento on assistencia.registo_cronologico (backwards compatibility)
+              concatMap(
+                (assistencia): Observable<Assistencia> => {
+                  if (assistencia.tecnico_user_id) {
+                    return this.usersAPI.get(assistencia.tecnico_user_id).pipe(
+                      map((res: User[]) => ({
+                        ...assistencia,
+                        tecnico: res[0].nome,
+                      }))
+                    );
+                  }
+
+                  const registoCronologico = clone(
+                    assistencia.registo_cronologico
+                  );
+                  const registoFiltered = registoCronologico
+                    .filter(
+                      (evento) =>
+                        evento.estado === 'em análise' ||
+                        evento.estado === 'contacto pendente' ||
+                        evento.estado === 'orçamento pendente' ||
+                        evento.estado === 'aguarda material' ||
+                        evento.estado === 'concluído' ||
+                        evento.estado === 'concluído s/ rep.'
+                    )
+                    .filter((evento) => evento.tecnico);
+                  const lastEvento = registoFiltered
+                    ? registoFiltered[registoFiltered.length - 1]
+                    : null;
+                  const tecnico = lastEvento ? lastEvento.tecnico : null;
+
+                  return of({ ...assistencia, tecnico });
+                }
+              )
+            )
+          )
+        ).pipe(toArray())
+      )
+    );
+
+  /**
+   * This Observable is a more performant version of
+   * fullyDetailedAssistencias$ because of .find() method
+   */
+  private minimallyDetailedAssistencias$ = (
+    assistenciasFromApi$: Observable<Assistencia[]>
+  ) =>
+    assistenciasFromApi$.pipe(
+      concatMap((assistenciasFromApi: Assistencia[]) =>
+        concat(
+          ...assistenciasFromApi.map((assistenciaFromApi) =>
+            this.usersAPI.get(assistenciaFromApi.cliente_user_id).pipe(
+              // retrieve client data
+              map((res: User[]) => res[0]),
+              map(
+                (cliente): Assistencia => ({
+                  ...assistenciaFromApi,
+                  cliente_user_name: cliente.nome,
+                  cliente_user_contacto: cliente.contacto,
+                })
+              ),
+
+              // get assistencia.tecnico from api or...
+              concatMap(
+                (assistencia): Observable<Assistencia> => {
+                  if (assistencia.tecnico_user_id) {
+                    return this.usersAPI.get(assistencia.tecnico_user_id).pipe(
+                      map(([{ nome }]) => ({
+                        ...assistencia,
+                        tecnico: nome,
+                      }))
+                    );
+                  }
+
+                  if (assistencia.id < 3300) {
+                    return of(assistencia);
+                  }
+
+                  // if (!assistencia.tecnico_user_id && assistencia.id < 3300) then from the last evento on assistencia.registo_cronologico (backwards compatibility)
+
+                  return of(assistencia).pipe(
+                    concatMap((assistencia) => {
+                      return this.get(assistencia.id).pipe(
+                        map(([assistencia]) => assistencia)
+                      );
+                    }),
+
+                    map((assistencia) => {
+                      return deserialize(assistencia, 'registo_cronologico');
+                    }),
+
+                    // retrieve tecnico data for all eventos of registo_cronologico
+                    concatMap((assistencia) => {
+                      return concat(
+                        ...assistencia.registo_cronologico.map((evento) => {
+                          if (evento.tecnico_user_id) {
+                            return this.usersAPI
+                              .get(evento.tecnico_user_id)
+                              .pipe(
+                                map(
+                                  (user: User[]): EventoCronologico => ({
+                                    ...evento,
+                                    tecnico: user[0].nome,
+                                  })
+                                )
+                              );
+                          }
+                          return of(evento);
+                        })
+                      ).pipe(
+                        toArray(),
+                        map(
+                          (registo_cronologico): Assistencia => ({
+                            ...assistencia,
+                            registo_cronologico,
+                          })
+                        )
+                      );
+                    }),
+                    map((assistencia) => {
+                      const registoCronologico = clone(
+                        assistencia.registo_cronologico
+                      );
+                      const registoFiltered = registoCronologico
+                        .filter(
+                          (evento) =>
+                            evento.estado === 'em análise' ||
+                            evento.estado === 'contacto pendente' ||
+                            evento.estado === 'orçamento pendente' ||
+                            evento.estado === 'aguarda material' ||
+                            evento.estado === 'concluído' ||
+                            evento.estado === 'concluído s/ rep.'
+                        )
+                        .filter((evento) => evento.tecnico);
+                      const lastEvento = registoFiltered
+                        ? registoFiltered[registoFiltered.length - 1]
+                        : null;
+                      const tecnico = lastEvento ? lastEvento.tecnico : null;
+
+                      return { ...assistencia, tecnico };
+                    })
                   );
                 }
-                return of(evento);
-
-              })).pipe(
-                toArray(),
-                map((registo_cronologico): Assistencia => ({ ...assistencia, registo_cronologico }))
               )
-            )),
+            )
+          )
+        ).pipe(toArray())
+      )
+    );
 
-            // retrieve editor data for all eventos of registo_cronologico
-            concatMap(assistencia => (
-              concat(...assistencia.registo_cronologico.map(evento => {
-                if (evento.editor_user_id) { // check because of backwards compatibility
-                  return this.usersAPI.get(evento.editor_user_id).pipe(
-                    map((user: User[]): EventoCronologico => ({ ...evento, editor: user[0].nome }))
-                  );
-                }
-                return of(evento);
-
-              })).pipe(
-                toArray(),
-                map((registo_cronologico): Assistencia => ({ ...assistencia, registo_cronologico }))
-              )
-            )),
-
-            // get assistencia.tecnico from api or from the last evento on assistencia.registo_cronologico (backwards compatibility)
-            concatMap((assistencia): Observable<Assistencia> => {
-              if (assistencia.tecnico_user_id) {
-                return this.usersAPI.get(assistencia.tecnico_user_id).pipe(
-                  map((res: User[]) => ({ ...assistencia, tecnico: res[0].nome }))
-                );
-              }
-
-              const registoCronologico = clone(assistencia.registo_cronologico);
-              const registoFiltered = registoCronologico
-                .filter(evento => (
-                  evento.estado === 'em análise' ||
-                  evento.estado === 'contacto pendente' ||
-                  evento.estado === 'orçamento pendente' ||
-                  evento.estado === 'aguarda material' ||
-                  evento.estado === 'concluído' ||
-                  evento.estado === 'concluído s/ rep.'
-                ))
-                .filter(evento => evento.tecnico);
-              const lastEvento = registoFiltered ? registoFiltered[registoFiltered.length - 1] : null;
-              const tecnico = lastEvento ? lastEvento.tecnico : null;
-
-              return of({ ...assistencia, tecnico });
-            })
-          ))
-        )).pipe(toArray())
-      ))
-    )
   constructor(
     protected feathersService: FeathersService,
-    private usersService: UsersService) {
+    private usersService: UsersService
+  ) {
     super(feathersService, 'assistencias');
   }
 
-
-  find(query?: object) {
-    return this.fullyDetailedAssistencias$(
-      super.find(query)
+  find(query?: any) {
+    return this.minimallyDetailedAssistencias$(
+      super.find({
+        ...query,
+        query: {
+          ...query.query,
+          $select: [
+            'id',
+            'cliente_user_id',
+            'tecnico_user_id',
+            'categoria',
+            'marca',
+            'modelo',
+            'cor',
+            'serial',
+            'problema',
+            // 'orcamento',
+            // 'relatorio_interno',
+            // 'relatorio_cliente',
+            // 'preco',
+            'estado',
+          ],
+        },
+      })
     );
   }
 
   get(id: number) {
-    return this.fullyDetailedAssistencias$(
-      super.get(id)
-    );
+    return this.fullyDetailedAssistencias$(super.get(id));
   }
 
   create(data: Assistencia, actionType?: string) {
@@ -135,34 +309,27 @@ export class AssistenciasApiService extends EntityApiAbstration {
         ...data,
         material: this.simplify(data.material),
         encomendas: this.simplify(data.encomendas),
-        messages: this.simplify(data.messages)
+        messages: this.simplify(data.messages),
       })
     );
   }
 
   patch(id: number, data: Assistencia, actionType?: string) {
     return this.fullyDetailedAssistencias$(
-      super.patch(
-        id,
-        {
-          ...data,
-          material: this.simplify(data.material),
-          encomendas: this.simplify(data.encomendas),
-          messages: this.simplify(data.messages)
-        }
-      )
+      super.patch(id, {
+        ...data,
+        material: this.simplify(data.material),
+        encomendas: this.simplify(data.encomendas),
+        messages: this.simplify(data.messages),
+      })
     );
   }
 
   onCreated() {
-    return this.fullyDetailedAssistencias$(
-      super.onCreated()
-    );
+    return this.fullyDetailedAssistencias$(super.onCreated());
   }
 
   onPatched() {
-    return this.fullyDetailedAssistencias$(
-      super.onCreated()
-    );
+    return this.fullyDetailedAssistencias$(super.onCreated());
   }
 }
